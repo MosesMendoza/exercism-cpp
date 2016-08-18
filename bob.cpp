fb7a730a007244b4533cbda9db0433c846b05757
Moses Mendoza <moses@puppet.com>
2016-08-18 16:46:54 -0700

    initial commit

---
+++ bob.cpp
@@ -0,0 +1,13 @@
+#include "bob.h"
+#include <iostream>
+
+namespace bob {
+  std::string hey(std::string input) {
+    if (input == "Tom-ay-to, tom-aaaah-to."){
+      return "Whatever.";
+    }
+    else {
+      return "foo";
+    }
+  }
+}
---
+++ bob.h
@@ -0,0 +1,5 @@
+#include <iostream>
+
+namespace bob {
+    std::string hey(std::string input);
+}
